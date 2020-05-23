eb9cca437ec6d2b5c7bcb59194f073656d5f83ea
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-05-23 18:58:59 +0200

    Created LeetCode project

---
+++ 001-first_non_repeating_character/src/Non_repeating_character.java
@@ -0,0 +1,12 @@
+/*
+Given a string, find the first non-repeating character in it. For example, if the input string is “eyeforaneye”,
+then the output should be ‘f’ and if the input string is “GeeksQuiz”, then the output should be ‘G’.
+ */
+
+public class Non_repeating_character {
+    public static void main(String[] args) {
+        String input1 = "eyeforaneye";      // f
+        String input2 = "GeeksQuiz";        // G
+        String input3 = "HelloBelloHey";    // B
+    }
+}
