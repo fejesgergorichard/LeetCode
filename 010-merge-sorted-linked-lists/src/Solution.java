3b55b16900b1f435514e916602e518890450be2b
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-05-30 00:25:41 +0200

    Merge a sorted, singly-linked list - 2 solutions

--- 010-merge-sorted-linked-lists/src/Solution.java
+++ 010-merge-sorted-linked-lists/src/Solution.java
@@ -9,7 +9,16 @@
  * }
  */
 
-// Recursive solution
+
+/* Merge two sorted linked lists and return it as a new sorted list.
+The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+
+/** Recursive solution */
 class Solution {
     public ListNode mergeTwoLists(ListNode A, ListNode B) {
         // If one of the lists is empty, we just return the other one (assumably not empty)
@@ -25,6 +34,40 @@ class Solution {
             B.next = mergeTwoLists(A, B.next);
             return B;
         }
+    }
+}
+
+/** Iterative solution */
+class Solution2 {
+    public ListNode mergeTwoLists(ListNode A, ListNode B) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode tail = dummyNode;
+
+        while (true) {
+            // If one of the lists is empty, we just strap on the other one and we're done
+            if (A == null) {
+                tail.next = B;
+                break;
+            }
+            if (B == null){
+                tail.next = A;
+                break;
+            }
+
+            // If the value in A node is the smaller one, we assign A as the next node and advance A by one node
+            if (A.val < B.val) {
+                tail.next = A;
+                A = A.next;
+            } else {
+                tail.next = B;
+                B = B.next;
+            }
+
+            // We advance the tail to become the last assigned node
+            tail = tail.next;
+        }
+        // dummyNode was set equal to the tail outside of the loop, so dummyNode is the parent of the tail node
+        return dummyNode.next;
     }
 }
 
