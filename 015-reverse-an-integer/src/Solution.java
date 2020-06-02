fa3983159990c55800cdf2067b19204384b9a91b
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-06-02 19:10:04 +0200

    Reverse an integer

--- 015-reverse-an-integer/src/Solution.java
+++ 015-reverse-an-integer/src/Solution.java
@@ -1,3 +1,24 @@
+/*
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+Input: 123
+Output: 321
+
+Example 2:
+Input: -123
+Output: -321
+
+Example 3:
+Input: 120
+Output: 21
+
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range:
+[−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the
+reversed integer overflows.
+ */
+
 class Solution {
     // Integer's max value is   2147483647 <-- max value/10 is  214748364
     // min value is            -2147483648 <-- min value/10 is -214748364
