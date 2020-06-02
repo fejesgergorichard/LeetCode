57946f43d95e3176360d58f2e309553a505bc090
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-06-02 19:08:51 +0200

    Reverse an integer

---
+++ 015-reverse-an-integer/src/Solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    // Integer's max value is   2147483647 <-- max value/10 is  214748364
+    // min value is            -2147483648 <-- min value/10 is -214748364
+    // If the current value of the solution is already bigger than the MAX_VALUE or MIN_VALUE then return 0
+    // If the current value of the solution is the MIN/MAX value/10 and the pop is greater than 7 or smaller than -8,
+    // the 32-bit int is going to overflow
+
+    /** Popping the last digit off the number and multiplying the number by 10 until x runs out  */
+    public int reverse(int x) {
+        int solution = 0, pop;
+
+        while (x != 0) {
+            pop = x % 10;
+            if ((solution > Integer.MAX_VALUE / 10) || solution == Integer.MAX_VALUE / 10 && pop > 7) return 0;
+            if ((solution < Integer.MIN_VALUE / 10) || solution == Integer.MIN_VALUE / 10 && pop < -8) return 0;
+            solution = solution * 10 + pop;
+            x /= 10;
+        }
+        return solution;
+    }
+
+    /** String manipulation version - fail */
+    public int reverseViaString(int x) {
+        int solution = 0;
+        char[] strChars = Integer.toString(x).toCharArray();
+        int n = strChars.length - 1;
+        for (int i = n; i >= 0; i--) {
+            int pop = (int) strChars[i] - 48;
+            if ((solution > Integer.MAX_VALUE / 10) || solution == Integer.MAX_VALUE / 10 && pop > 7) return 0;
+            if ((solution < Integer.MIN_VALUE / 10) || solution == Integer.MIN_VALUE / 10 && pop < -8) return 0;
+            solution = solution * 10 + pop;
+        }
+
+        return solution;
+    }
+}
