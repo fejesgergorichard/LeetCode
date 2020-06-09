29714c8b868ac4fa38d324921f14ca4bd61bfb9d
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-06-09 13:32:46 +0200

    primitive array solution - much better runtime
    ArrayList - 5 ms
    int[] - 2 ms

--- 026-decompress-run-length-encoded-list/src/Solution.java
+++ 026-decompress-run-length-encoded-list/src/Solution.java
@@ -1,4 +1,26 @@
 class Solution {
+    /** Primitive array solution - better runtime */
+    public int[] decompressRLElist(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            if (i % 2 == 0) count += nums[i];
+        }
+        System.out.println(count);
+        
+        int[] solution = new int[count];
+        count = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            for (int j = count; j < count + nums[i]; j++) {
+                solution[j] = nums[i+1];
+            }
+            count += nums[i];
+        }
+        
+        return solution;
+    }
+
+
+    /** ArrayList solution - simpler code, slower runtime */
     public int[] decompressRLElist(int[] nums) {
         ArrayList<Integer> list = new ArrayList<>();
         
