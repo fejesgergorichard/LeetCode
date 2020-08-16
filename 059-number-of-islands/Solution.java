a5f41d1e6d8dd1acdd4a727282a417bff34f9991
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-08-16 14:03:14 +0200

    Number of islands - medium

---
+++ 059-number-of-islands/Solution.java
@@ -0,0 +1,54 @@
+/* Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3 */
+
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    destroyIsland(grid, i, j);
+                }
+                
+            }
+        }
+        return count;
+        
+    }
+    
+    public void destroyIsland(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
+            return;
+        }
+        
+        grid[i][j] = '0';
+        
+        destroyIsland(grid, i-1, j); // up
+        destroyIsland(grid, i+1, j); // dn
+        destroyIsland(grid, i, j-1); // left
+        destroyIsland(grid, i, j+1); // right
+        
+    }
+}
